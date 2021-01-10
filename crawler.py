e00b7f9f6f4eb99f628235daa2b7120fda590969
Liangyu Huang <liangyuhuang@outlook.com>
2021-01-10 14:49:31 +0800

    init

---
+++ crawler.py
@@ -0,0 +1,9 @@
+# Author: Liangyu Huang
+# Python 3.6.12
+
+# -*- coding: utf-8 -*-
+import requests
+from urllib.parse import urlencode
+from requests.exceptions import RequestException
+from bs4 import BeautifulSoup
+
